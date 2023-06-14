20730e9855597d2cfd8d349a0912fe8422eb10d0
Wagner Oliveira <contato-wagneros@outlook.com>
2023-06-14 12:48:42 -0300

    Gráfico atualizável

---
+++ script/script.js
@@ -0,0 +1,40 @@
+import imprimeCotacao from "./imprimeCotacao.js";
+
+const graficoDolar = document.querySelector('#graficoDolar')
+
+const graficoParaDolar = new Chart(graficoDolar, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [{
+        label: 'Dólar',
+        data: [],
+        borderWidth: 1
+      }]
+    },
+  });
+
+async function conectaAPI(){
+    const conecta = await fetch ('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+    const conectaTraduzido = await conecta.json()
+    let tempo = geraHorario()
+    let valor = conectaTraduzido.USDBRL.ask
+    adicionarDados(graficoParaDolar, tempo, valor)
+    imprimeCotacao("dolar", valor)
+}
+
+setInterval(() => conectaAPI(), 5000)
+
+function geraHorario(){
+    let data = new Date()
+    let horario = data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds()
+    return horario
+}
+
+function adicionarDados(grafico, legenda, dados){
+    grafico.data.labels.push(legenda)
+    grafico.data.datasets.forEach((dataset) =>{
+        dataset.data.push(dados)
+    })
+    grafico.update()
+}
